831f0f623c706fb3ede26ec69cc9642e029fd10e
Olha Tverdokhlib <balabolya@gmail.com>
2023-06-26 01:16:03 +0200

    changed if to switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,13 +10,20 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  
   const countFeedback = item => {
-    if (item === 'good') {
-      setGood(prevGood => prevGood + 1);
-    } else if (item === 'neutral') {
-      setNeutral(prevNeutral => prevNeutral + 1);
-    } else if (item === 'bad') {
-      setBad(prevBad => prevBad + 1);
+    switch (item) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
     }
   };
 
